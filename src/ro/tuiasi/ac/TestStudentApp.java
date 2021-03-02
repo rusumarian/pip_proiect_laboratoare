96ab500a42e7355702fdb5ebb46a169842d980e4
Marian Rusu <rusu.marian@ac.tuiasi.ro>
2021-03-03 00:38:09 +0200

    Am afisat 3 studenti

--- src/ro/tuiasi/ac/TestStudentApp.java
+++ src/ro/tuiasi/ac/TestStudentApp.java
@@ -7,9 +7,11 @@ public class TestStudentApp {
 		//declares a student
 		Student s1 = new Student(10,"a","b");
 		Student s2 = new Student(33,"Popescu","Ion");
+		Student s3 = new Student(34,"Ionica","Laur");
 		
 		s1.print();
 		s2.print();
+		System.out.println(s3);
 		
 	}
 
